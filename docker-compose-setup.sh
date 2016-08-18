c5b711398cf6bd4a59ce55c80286c0e9ac70b6de
Herman Tolentino <herman.tolentino@gmail.com>
2016-08-18 00:57:22 +0000

    update docker-compose set up script

--- docker-compose-setup.sh
+++ docker-compose-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get install -y curl
 curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > docker-compose
 chmod a+x docker-compose
 sudo mv docker-compose /usr/local/bin/docker-compose
